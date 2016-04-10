208b5671929e3c0aaf4c57bc935eb264933f3508
Eric Vinicius (Vinicius Saw) <ericvinicius541@hotmail.com>
2016-04-10 17:43:58 +0000

    The main.cpp
    The main.cpp file.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <windows.h>
+
 int main(){
 	
 	HKEY chave;
 	
 	char nome[30];
 	printf("Digite exatamente o nome da fonte:\n");
-	scanf("%30s",&nome); // Obtém um máximo de 30 caracteres
+	scanf("%30s",&nome); // Obtém um máximo de 30 caracteres & atribui a variável $nome
 	
 	char fonte[30];
 	printf("Digite exatamente o nome da fonte + a extensao:\n");
-	scanf("%30s",&fonte);
+	scanf("%30s",&fonte); // Obtém um máximo de 30 caracteres & atribui a variável $fonte
 	
-	RegOpenKey(HKEY_LOCAL_MACHINE,"SOFTWARE\\Microsoft\\WINDOWS NT\\CurrentVersion\\Fonts",&chave);
-	RegSetValueEx(chave,(nome),0,REG_SZ,(LPBYTE)fonte,strlen(fonte));
+	RegOpenKey(HKEY_LOCAL_MACHINE,"SOFTWARE\\Microsoft\\WINDOWS NT\\CurrentVersion\\Fonts",&chave); // Define o diretório no Registro do Windows na variável $chave
+	
+	RegSetValueEx(chave,(nome),0,REG_SZ,(LPBYTE)fonte,strlen(fonte);) 
 	//RegCreateKey(HKEY_CURRENT_USER,"\\AppEvents\\Teste\\SubConjunto01",&chave);
-	RegCloseKey(chave);
+	RegCloseKey(chave); // Fecha a chave;
 	return 0;
 }
